6ac1541f9af859d9eae0e7fde5d688d55570d73c
André P. Bertoletti <apbertoletti@gmail.com>
2017-09-03 17:39:04 -0300

    Coding Dojo realizado no encontro do grupo ArarasDev, em 2/9/2017, na UniAraras

---
+++ FizzBuzz/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FizzBuzz")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FizzBuzz")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a13c6623-2336-4a84-b983-477e04f1ea5d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FizzBuzz/UnitTest1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace FizzBuzz
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void Retorna_FizzBuzz_quando_passar_30()
+        {
+            Assert.AreEqual("FizzBuzz", FizzBuzz(30));
+        }
+
+        [TestMethod]
+        public void Retorna_1_quando_passar_1()
+        {
+            Assert.AreEqual("1", FizzBuzz(1));
+        }
+
+        [TestMethod]
+        public void Retorna_2_quando_passar_2()
+        {
+            Assert.AreEqual("2", FizzBuzz(2));
+        }
+
+        [TestMethod]
+        public void Retorna_Fizz_quando_passar_3()
+        {
+            Assert.AreEqual("Fizz", FizzBuzz(3));
+        }
+
+        [TestMethod]
+        public void Retorna_Buzz_quando_passar_5()
+        {
+            Assert.AreEqual("Buzz", FizzBuzz(5));
+        }
+
+        [TestMethod]
+        public void Retorna_FizzBuzz_quando_passar_divisivel_por_3_e_5()
+        {
+            Assert.AreEqual("FizzBuzz", FizzBuzz(15));
+        }
+
+
+        [TestMethod]
+        public void TesteListarTodos()
+        {
+            ListarTodos();
+        }
+
+        void ListarTodos()
+        {
+            var lista = Enumerable.Range(1, 100).Select(FizzBuzz).ToArray();
+
+            foreach (var elemento in lista)
+            {
+                Console.Write(elemento + ", ");
+            }
+        }
+
+        string FizzBuzz(int numero)
+        {
+            var result = string.Format("{0}{1}",
+                numero % 3 == 0 ? "Fizz" : string.Empty,
+                numero % 5 == 0 ? "Buzz" : string.Empty
+                );
+
+            return string.IsNullOrEmpty(result) ? numero.ToString() : result;
+        }
+    }
+}
